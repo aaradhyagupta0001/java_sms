95033bbe8e2bbce5f8586e538cbd425c1b3b593d
Ankur Gupta <ankurdotcom@gmail.com>
2025-02-05 21:34:05 +0530

    my first java

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main{
+   
+    public static void main(String[] args) {
+        System.out.println("this is my shop management system");
+
+        int a=98;
+        int b=2;
+        int ans=a+b;
+        System.out.println(ans);
+
+        int minus=a-b;
+        System.out.println(minus);
+    }
+}
